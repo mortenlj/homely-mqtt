325a2acec84623781383af4bae4898e7f0236e25
Morten Lied Johansen <mortenjo@ifi.uio.no>
2023-08-10 12:49:27 +0200

    Change to pydantic-settings v2 from pydantic v1

--- ibidem/homely_mqtt/config.py
+++ ibidem/homely_mqtt/config.py
@@ -1,20 +1,16 @@
 from enum import Enum
+from typing import Optional
 
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Mode(str, Enum):
     DEBUG = "Debug"
     RELEASE = "Release"
 
-    def __new__(cls, value):
-        return super().__new__(cls, value.lower())
-
 
 class Settings(BaseSettings):
-    class Config:
-        env_prefix = "HOMELY_MQTT_"
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_prefix="HOMELY_MQTT_", env_file=".env")
 
     mode: Mode = Mode.DEBUG
     bind_address: str = "127.0.0.1"
