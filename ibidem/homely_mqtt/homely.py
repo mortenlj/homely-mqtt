94269fa56acb73221d29e968dd956190cb5a6930
Morten Lied Johansen <mortenjo@ifi.uio.no>
2023-12-06 14:45:52 +0100

    Nicer slugs for devices

--- ibidem/homely_mqtt/homely.py
+++ ibidem/homely_mqtt/homely.py
@@ -34,8 +34,28 @@ class Device(BaseModel):
     location: str
 
     @property
+    def floor(self):
+        split_idx = self.location.rfind("-")
+        if split_idx > 0:
+            floor = self.location[:split_idx].strip()
+            floor_number = int(floor.split()[-1])
+            if floor_number == 0:
+                return "Ground"
+            if floor_number < 0:
+                return "Basement"
+            return str(floor_number + 1)
+        return self.location
+
+    @property
+    def room(self):
+        split_idx = self.location.rfind("-")
+        if split_idx > 0:
+            return self.location[split_idx+1:].strip()
+        return self.location
+
+    @property
     def slug(self):
-        return slugify(f"{self.name} {self.location}")
+        return slugify(f"{self.name} {self.floor} {self.room}", separator="_")
 
 
 class Measurement(BaseModel):
---
+++ tests/test_device.py
@@ -0,0 +1,33 @@
+from collections import namedtuple
+from uuid import uuid4
+
+import pytest
+
+from ibidem.homely_mqtt.homely import Device
+
+FloorData = namedtuple("FloorData", ["expected", "input"])
+
+
+class TestDevice:
+
+    @pytest.fixture(params=[
+        FloorData("Basement", "Floor -1"),
+        FloorData("Ground", "Floor 0"),
+        FloorData("2", "Floor 1"),
+    ])
+    def floor(self, request):
+        return request.param
+
+    @pytest.fixture(params=["Kitchen", "Living room", "Bedroom"])
+    def room(self, request):
+        return request.param
+
+    @pytest.fixture
+    def device(self, floor, room):
+        return Device(id=uuid4(), name="Test Device", location=f"{floor.input} - {room}")
+
+    def test_floor(self, device, floor):
+        assert device.floor == floor.expected
+
+    def test_room(self, device, room):
+        assert device.room == room
